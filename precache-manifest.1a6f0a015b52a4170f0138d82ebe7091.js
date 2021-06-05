15f29389097a527ffaa1750553a88da185282685
yechuanjie <1102684711@qq.com>
2021-06-05 16:56:22 +0000

    Update

---
+++ precache-manifest.1a6f0a015b52a4170f0138d82ebe7091.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a0bec1ddb0ce95b1845",
+    "url": "css/app.b7a7f9fd.css"
+  },
+  {
+    "revision": "2378960834bec8475f9ce916a4a1b39b",
+    "url": "icon.png"
+  },
+  {
+    "revision": "d90e8dc8646adebe46a8bbc856bc87f5",
+    "url": "index.html"
+  },
+  {
+    "revision": "1a0bec1ddb0ce95b1845",
+    "url": "js/app.fd017537.js"
+  },
+  {
+    "revision": "8e1a14798f2adb6d6e0c",
+    "url": "js/chunk-vendors.1d6740d5.js"
+  },
+  {
+    "revision": "feddd58abbc842907700c0deb77e12c6",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
